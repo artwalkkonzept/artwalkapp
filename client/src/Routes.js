858a7651903f41681ca82d22e9f0cb07ac9d6a8f
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-11-12 16:51:00 +0100

    Still can't get data

---
+++ client/src/Routes.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { LogInPage } from './pages/LogInPage';
+import { SignUpPage } from './pages/SignUpPage';
+import { UserInfoPage } from './pages/UserInfoPage';
+import { PrivateRoute } from './auth/PrivateRoute';
+
+import Logo from "./logo/Logo";
+
+export const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <PrivateRoute path="/" exact>
+                    <UserInfoPage />
+                </PrivateRoute>
+                <Route path="/login">
+                    <LogInPage />
+                </Route>
+                <Route path="/signup">
+                    <SignUpPage />
+                </Route>
+            </Switch>
+            <Logo />
+        </Router>
+    );
+}
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controller/file.controller");
+import { logInRoute } from './logInRoute';
+import { signUpRoute } from './signUpRoute';
+import { testRoute } from './testRoute';
+import { updateUserInfoRoute } from './updateUserInfoRoute';
 
-let routes = (app) => {
-  router.post("/upload", controller.upload);
-  router.get("/files", controller.getListFiles);
-  router.get("/files/:name", controller.download);
-
-  app.use(router);
-};
-
-module.exports = routes;
+export const routes = [
+    logInRoute,
+    signUpRoute,
+    testRoute,
+    updateUserInfoRoute,
+];
