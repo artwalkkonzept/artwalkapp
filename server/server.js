893db257b955906df5abf57be4e85cfd3883c9da
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-11-12 09:32:34 +0100

    Update and styling

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ require("./app/routes/artwalk.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.MONGO_URL || 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
