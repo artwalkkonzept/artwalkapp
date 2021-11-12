9ec6d32049ab48bb19604265001528e81d15046b
@artwalkkonzept <artwalkkonzept@gmail.com>
2021-11-12 09:24:02 +0100

    Update and styling

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dbConfig = require("./app/config/db.config");
+//const dbConfig = require("./app/config/db.config");
 
 const mongoose = require("mongoose");
 const app = express();
@@ -59,7 +59,7 @@ require("./app/routes/artwalk.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.MONGO_URL || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
